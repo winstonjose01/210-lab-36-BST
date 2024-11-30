a0c9ab2e632417f54b1515a71899dd3a0f751d4f
winstonjose01 <winston.jose@gmail.com>
2024-11-29 21:26:55 -0800

    revision-1

---
+++ StringBinaryTree.h
@@ -0,0 +1,49 @@
+// Specification file for the IntBinaryTree class
+#ifndef STRINGBINARYTREE_H
+#define STRINGBINARYTREE_H
+
+
+#include <string>
+using namespace std;
+
+// The IntBinaryTree class manages a binary tree of integers.
+class StringBinaryTree {
+private:
+   // TreeNode is a private structure for IntBinaryTree nodes.
+   struct TreeNode {
+      string value;         // The value in the node
+      TreeNode *left;    // Pointer to left child node
+      TreeNode *right;   // Pointer to right child node
+   };
+
+   // root points to the topmost node of the tree.
+   TreeNode *root;      
+
+   // Private member functions for internal operations.
+   void insert(TreeNode *&, TreeNode *&);
+   void destroySubTree(TreeNode *);
+   void deleteNode(string, TreeNode *&);
+   void makeDeletion(TreeNode *&);
+   void displayInOrder(TreeNode *) const;
+   void displayPreOrder(TreeNode *) const;
+   void displayPostOrder(TreeNode *) const;
+
+public:
+   // Constructor initializes the root to nullptr, indicating an empty tree.
+   StringBinaryTree()     { root = nullptr; }
+
+   // Destructor deallocates all nodes in the tree.
+   ~StringBinaryTree()    { destroySubTree(root); }
+
+   // Public interface for inserting, searching, and removing nodes.
+   void insertNode(string);
+   bool searchNode(string);
+   void remove(string);
+
+   // Public wrappers for tree traversal functions.
+   void displayInOrder() const     {  displayInOrder(root); }
+   void displayPreOrder() const    {  displayPreOrder(root); }
+   void displayPostOrder() const   {  displayPostOrder(root); }
+};
+
+#endif // STRINGBINARYTREE_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
-#include <iostream>
+#include <fstream>
 #include "IntBinaryTree.h"
+#include "StringBinaryTree.h"
+#include <iostream>
+#include <cstdlib>
 using namespace std;
 
 int main() {
+    srand(time(0));
     
+    string line;
+    int numline = 0;
+    ifstream fin("codes.txt");
+    StringBinaryTree codes;
+    
+    if (!fin.is_open()){
+        cout << "Error opening file. Aborting!";
+        return(1);
+    }
+    else{
+        while (getline(fin,line)){
+            codes.insertNode(line);
+            numline++;
+        }
+    }
+    fin.clear();
+    fin.seekg(0);
 
     return 0;
 }
