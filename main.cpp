4802074221757a58075b1cc655113e87c1f20d52
winstonjose01 <winston.jose@gmail.com>
2024-11-30 08:13:58 -0800

    revision-3

--- main.cpp
+++ main.cpp
@@ -21,12 +21,53 @@ int main() {
     else{
         while (getline(fin,line)){
             codes.insertNode(line);
-            cout << line << endl;
-            numline++;
         }
     }
     fin.clear();
     fin.seekg(0);
-    cout << numline;
+    int choice = 0;
+    cout << "The binary search tree has been populated." << endl;
+    while (choice < 7){
+        string record;
+        cout << endl;
+        cout << "Choose an option:" << endl;
+        cout << "[1] Add a record " << endl;
+        cout << "[2] Delete a record " << endl;
+        cout << "[3] Search for a record " << endl;
+        cout << "[4] Modify a record" << endl;
+        cout << "[5] Quit" << endl;
+        cout << " Option ---> ";
+        cin >> choice;
+        switch (choice){
+            case 1: 
+                cout << "Enter the record to add: ";
+                cin >> record;
+                codes.insertNode(record);
+                break;
+            case 2:
+                cout << "Enter the record to delete: ";
+                cin >> record;
+                codes.remove(record);
+                break;
+            case 4:
+                cout << "Display tree InOrder";
+                codes.displayInOrder();
+                break;
+            case 5:
+                cout << "Display tree PreOrder";
+                codes.displayPreOrder();
+                break;
+            case 6:
+                cout << "Display tree PostOrder";
+                codes.displayPreOrder();
+                break;
+            case 7: 
+                exit(1);
+                break;
+            
+        }
+    }
+
+
     return 0;
 }
